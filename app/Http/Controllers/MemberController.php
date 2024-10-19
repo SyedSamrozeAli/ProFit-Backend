74b199e7d89c257ccdefcb17832c180778216b58
Syed Samroze Ali <samroze3@gmail.com>
2024-10-19 20:16:45 +0500

    Mmeber controller updated

--- app/Http/Controllers/MemberController.php
+++ app/Http/Controllers/MemberController.php
@@ -19,36 +19,35 @@ class MemberController extends Controller
                 $membership_duration = $request->membership_duration;
 
                 // Inserting member data into members table
-                DB::insert("
-                INSERT INTO members 
-                (name, member_email, phone_number, address, age, CNIC, DOB, trainer_id, height, weight, bmi, membership_type, profile_image, health_issues, user_status, addmission_date, membership_start_date, membership_end_date)
-                VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)
-            ", [
-                    $request->name,
-                    $request->member_email,
-                    $request->phone_number,
-                    $request->address,
-                    $request->age,
-                    $request->CNIC,
-                    $request->DOB,
-                    $request->trainer_id,
-                    $request->height,
-                    $request->weight,
-                    $BMI,
-                    $request->membership_type,
-                    $request->profile_image,
-                    $request->health_issues,
-                    'active',
-                    $request->addmission_date,
-                    now(),
-                    now()->addDays($membership_duration * 30)
-                ]);
+                DB::insert(
+                    "INSERT INTO members 
+                    (name, member_email, phone_number, address, age, CNIC, DOB, trainer_id, height, weight, bmi, profile_image, health_issues, user_status, addmission_date)
+                    VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)"
+                    ,
+                    [
+                        $request->name,
+                        $request->member_email,
+                        $request->phone_number,
+                        $request->address,
+                        $request->age,
+                        $request->CNIC,
+                        $request->DOB,
+                        $request->trainer_id,
+                        $request->height,
+                        $request->weight,
+                        $BMI,
+                        $request->profile_image,
+                        $request->health_issues,
+                        'active',
+                        $request->addmission_date
+                    ]
+                );
 
                 // Getting the newly added member's ID using CNIC
                 $newMember = DB::select("SELECT member_id FROM members WHERE CNIC=?", [$request->CNIC]);
 
                 // Setting price according to membership duration and membership type
-                if ($request->membership_type == 'premium') {
+                if ($request->membership_type == 'Premium') {
 
                     if ($membership_duration == 3) {
                         $price = 52000;  // 52k for 3 months
@@ -59,10 +58,11 @@ class MemberController extends Controller
                     }
 
                     // Adding the newly created member to the trainer's list of members
+
                     DB::insert("INSERT INTO trainers_have_members (trainer_id, member_id) VALUES (?,?)", [$request->trainer_id, $newMember[0]->member_id]);
 
 
-                } else {
+                } else if ($request->membership_type == 'Standard') {
                     if ($membership_duration == 3) {
                         $price = 15000;  // 15k for 3 months 
                     } else if ($membership_duration == 6) {
@@ -72,13 +72,14 @@ class MemberController extends Controller
                     }
                 }
 
-                // Adding new memberships for the newly added member
-                DB::insert("
-                INSERT INTO membership (membership_type, member_id, price, duration, start_date, end_date, status)
+                $membership = DB::select("SELECT membership_id FROM memberships WHERE membership_type = ?", [$request->membership_type]);
+
+                // Linking members with their memberships
+                DB::insert("INSERT INTO member_has_membership (member_id,membership_id, price, duration, start_date, end_date, status)
                 VALUES(?,?,?,?,?,?,?)
             ", [
-                    $request->membership_type,
                     $newMember[0]->member_id,
+                    $membership[0]->membership_id,
                     $price,
                     (string) $membership_duration,
                     now(),
@@ -86,12 +87,7 @@ class MemberController extends Controller
                     'active'
                 ]);
 
-                // Getting the newly added membership's ID
-                $membership = DB::select("SELECT membership_id FROM membership WHERE member_id=?", [$newMember[0]->member_id]);
-
-                // Updating the membership_id in members for the newly created member
                 DB::update("UPDATE members SET membership_id=? WHERE member_id=?", [$membership[0]->membership_id, $newMember[0]->member_id]);
-
 
             });
 
