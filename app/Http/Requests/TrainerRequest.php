1da5d6aa691b8068cf9158aebabfe43926c33459
Syed Samroze Ali <samroze3@gmail.com>
2024-11-10 04:15:02 +0500

    CNIC uniquness added

--- app/Http/Requests/TrainerRequest.php
+++ app/Http/Requests/TrainerRequest.php
@@ -33,15 +33,16 @@ class TrainerRequest extends FormRequest
 
             // Validations for Creating a new trainer
             case 'POST':
+                $trainerId = $this->route('trainerId'); // Assuming the trainer ID is passed in the route
                 return [
                     'trainer_name' => 'required|string|min:3|max:50',
                     'trainer_email' => 'required|email|unique:trainers,trainer_email',
-                    'CNIC' => 'required|string|min:13|max:13',
+                    'CNIC' => ['required|string|min:13|max:13|unique:trainers,CNIC' . $trainerId . ',trainer_id'],
                     'gender' => 'required|string|in:male,female',
                     'DOB' => 'required|date|before_or_equal:2010-01-01',
                     'phone_number' => 'required|string|min:11|max:11|unique:trainers,phone_number',
                     'trainer_profile_image' => 'string|unique:trainers',
-                    'trainer_address' => 'required|string|min:10|max:100',
+                    'trainer_address' => 'required|string|min:10|max:255',
                     'experience' => 'required|integer|min:1|max:100',
                     'salary' => 'required|numeric|min:1000',
                 ];
@@ -54,13 +55,12 @@ class TrainerRequest extends FormRequest
                 return [
                     'trainer_name' => 'string|min:3|max:50',
                     'trainer_email' => ['email', 'unique:trainers,trainer_email,' . $trainerId . ',trainer_id'], // trainerId is concatenated so that unique values are check by ignoring the current trainerId
-                    'CNIC' => 'string',// we have to add unique constraint and 13 characters constraint in CNIC but in the end.
-                    'age' => 'integer|max:50',
+                    'CNIC' => ['string|min:13|max:13|unique:trainers,CNIC' . $trainerId . ',trainer_id'],
                     'gender' => 'string|in:male,female',
-                    'DOB' => 'date|after_or_equal:2010-01-01',
+                    'DOB' => 'date|before_or_equal:2010-01-01',
                     'phone_number' => 'string|min:10|max:15|unique:trainers,phone_number',
                     'trainer_profile_image' => 'string|unique:trainers',
-                    'trainer_address' => 'string|min:10|max:100',
+                    'trainer_address' => 'string|min:10|max:255',
                     'experience' => 'integer|min:1|max:20',
                     'salary' => 'numeric|min:1000',
                 ];
