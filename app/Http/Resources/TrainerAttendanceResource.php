ebc5e57fe00553e5e759c72d16e1ae00f6caf1ed
Syed Samroze Ali <samroze3@gmail.com>
2024-11-17 18:45:17 +0500

    Trainer name added

--- app/Http/Resources/TrainerAttendanceResource.php
+++ app/Http/Resources/TrainerAttendanceResource.php
@@ -15,9 +15,9 @@ class TrainerAttendanceResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
-            'attendance_id' => $this->trainer_attendance_id,
-            'trainer_id' => $this->trainer_id,
             'attendance_date' => $this->attendance_date,
+            'trainer_id' => $this->trainer_id,
+            'trainer_name' => $this->trainer_name,
             'check_in_time' => $this->check_in_time,
             'check_out_time' => $this->check_out_time,
             'attendance_status' => $this->attendance_status,
