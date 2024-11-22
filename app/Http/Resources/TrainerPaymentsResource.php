c4a1a8b01a6fb836c361935e0d72d2176c1e602e
Syed Samroze Ali <samroze3@gmail.com>
2024-11-22 23:54:09 +0500

    Trainer payments resource added

---
+++ app/Http/Resources/TrainerPaymentsResource.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class TrainerPaymentsResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+
+
+        return [
+            'trainer_payment_id' => $this->trainer_payment_id,
+            'trainer_id' => $this->trainer_id,
+            'trainer_name' => $this->trainer_name,
+            'payment_date' => $this->payment_date,
+            'payment_amount' => $this->payment_amount,
+            'paid_amount' => $this->paid_amount,
+            'dues' => $this->dues,
+            'balance' => $this->balance,
+            'payment_method' => $this->payment_method,
+            'payment_status' => $this->payment_status == 1 ? "completed" : "pending",
+
+        ];
+    }
+}
