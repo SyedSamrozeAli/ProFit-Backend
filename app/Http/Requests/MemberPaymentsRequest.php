273ea274af702b16a2676f42a3dff81efe542cd5
Syed Samroze Ali <samroze3@gmail.com>
2024-11-22 19:27:48 +0500

    Member payments request updated

---
+++ app/Http/Requests/MemberPaymentsRequest.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Http\Exceptions\HttpResponseException;
+class MemberPaymentsRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        $method = $this->method();
+        switch ($method) {
+
+            case 'POST':
+                return [
+                    'member_id' => 'required|exists:members,member_id',
+                    'membership_type' => 'required|exists:memberships,membership_type',
+                    'payment_date' => 'required|date',
+                    'payment_amount' => 'required|numeric|min:0',
+                    'payment_method' => 'required|in:cash,online',
+                    'paid_amount' => 'required|numeric|min:0',
+                ];
+            case 'PUT':
+                return [
+                    'payment_date' => 'date',
+                    'payment_amount' => 'required|numeric|min:0',
+                    'payment_method' => 'in:cash,online',
+                    'paid_amount' => 'required|numeric|min:0',
+                ];
+            case 'GET':
+
+                return [
+                    'month' => 'required|integer|min:1|max:12',
+                    'year' => 'required|integer|min:2010|max:2400',
+                ];
+
+            default:
+                return [];
+        }
+
+    }
+
+    protected function failedValidation(Validator $validator)
+    {
+        // Get all error messages without field keys
+        $errorMessages = $validator->errors()->all();
+
+        throw new HttpResponseException(response()->json([
+            'success' => false,
+            'status_code' => 422,
+            'message' => 'Validation errors occurred.',
+            'errors' => $errorMessages, // Returning only the list of error messages
+            'data' => []
+        ], 422));
+    }
+}
