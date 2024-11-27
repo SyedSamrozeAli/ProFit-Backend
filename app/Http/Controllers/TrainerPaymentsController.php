ada0a4e32feb61d9a0fce44eed333135622a9001
Syed Samroze Ali <samroze3@gmail.com>
2024-11-27 13:02:32 +0500

    payment reciept error corrected

--- app/Http/Controllers/TrainerPaymentsController.php
+++ app/Http/Controllers/TrainerPaymentsController.php
@@ -32,7 +32,7 @@ class TrainerPaymentsController extends Controller
             }
 
             // Adding payment reciept image
-            if ($req->has('payment_reciept')) {
+            if ($req->has('payment_reciept') && $req->payment_reciept != null) {
                 $paymentReciept = $req->file('payment_reciept');
                 $imageName = time() . '.' . $paymentReciept->getClientOriginalExtension();
                 $paymentReciept->move('images/trainer/paymentsReciepts', $imageName);
