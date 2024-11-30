a48f9e49f4fd9b3f6040c9ab0adcdc3944bdbbbe
Syed Samroze Ali <samroze3@gmail.com>
2024-11-30 23:52:04 +0500

    Dashboard functions added

--- app/Models/MemberAttendance.php
+++ app/Models/MemberAttendance.php
@@ -71,4 +71,43 @@ class MemberAttendance extends Model
             [$timeIn, $timeOut, $status, $memberId, $date]
         );
     }
+
+    static public function getMemberAttendanceRate()
+    {
+        $attendanceRate = DB::select(
+            "SELECT 
+                (SUM(CASE WHEN attendance_status = 'present' THEN 1 ELSE 0 END) / COUNT(*)) * 100 AS attendance_rate
+            FROM members_attendance
+            WHERE attendance_date BETWEEN DATE_FORMAT(CURRENT_DATE(), '%Y-%m-01') AND LAST_DAY(CURRENT_DATE())
+            "
+        )[0]->attendance_rate;
+
+        return $attendanceRate ?? 0; // Default to 0 if no data exists
+    }
+
+    static public function getMemberAttendanceGrowthRate()
+    {
+        $currentMonth = DB::select(
+            "SELECT 
+                    SUM(CASE WHEN attendance_status = 'present' THEN 1 ELSE 0 END) AS present_days,
+                    COUNT(*) AS total_days
+                FROM members_attendance
+                WHERE attendance_date BETWEEN DATE_FORMAT(CURRENT_DATE(), '%Y-%m-01') AND LAST_DAY(CURRENT_DATE())
+                "
+        );
+
+        $previousMonth = DB::select(
+            "SELECT 
+                    SUM(CASE WHEN attendance_status = 'present' THEN 1 ELSE 0 END) AS present_days,
+                    COUNT(*) AS total_days
+                FROM members_attendance
+                WHERE attendance_date BETWEEN DATE_FORMAT(DATE_SUB(CURRENT_DATE(), INTERVAL 1 MONTH), '%Y-%m-01') 
+                AND LAST_DAY(DATE_SUB(CURRENT_DATE(), INTERVAL 1 MONTH))"
+        );
+
+        return [
+            "currentMonth" => $currentMonth,
+            "previousMonth" => $previousMonth,
+        ];
+    }
 }
